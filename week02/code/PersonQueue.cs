b9d1df5604ef2910d92ed01eec9aa4b2dbdc7557
KobeLemon <kobe42lemon@outlook.com>
2024-03-11 20:59:47 -0600

    Finished taking turns queueing

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -11,7 +11,8 @@ public class PersonQueue {
     /// </summary>
     /// <param name="person">The person to add</param>
     public void Enqueue(Person person) {
-        _queue.Insert(0, person);
+        // _queue.Insert(0, person);
+        _queue.Add(person); // change to ".Add" instead of ".Insert"
     }
 
     public Person Dequeue() {
